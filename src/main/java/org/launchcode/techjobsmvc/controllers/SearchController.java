ecb7b9b0792ee2e1ccc0dc47bed07e92ea25c2bb
Alex Merchant <unconfigured@noexist.com>
2023-11-06 13:01:01 -0600

    FINISHED REGULAR ASSIGNMENT REQUIREMENTS
    * Added new controller method 'displaySearchResults' to handle search
        queries on the job data set.
    * Added support for empty searchTerm and 'all' top return all jobs in
        job data set.
    * Added support to search all column values for all jobs when user
        selects 'All' searchType
    * Added support to handle a specified searchType and searchTerm to only
        retrieve the relevant jobs from the job data set.

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,15 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -22,6 +28,31 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping(value = "results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam(required = false) String searchTerm) {
+        ArrayList<Job> jobs;
+        String titleColumnLabel = searchType;
+
+        model.addAttribute("columns", columnChoices);
+
+        if (searchTerm.trim().equalsIgnoreCase("all") || searchTerm.trim().isEmpty()) {
+            jobs = JobData.findAll();
+        } else if (searchType.equals("all")) {
+            jobs = JobData.findByValue(searchTerm.trim().toLowerCase());
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm.trim().toLowerCase());
+        }
+
+        if (titleColumnLabel.equalsIgnoreCase("positionType")) {
+            titleColumnLabel = "Position Type";
+        } else if (titleColumnLabel.equalsIgnoreCase("coreCompetency")) {
+            titleColumnLabel = "Skill";
+        }
+
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("title", "Jobs with " + titleColumnLabel + ": " + searchTerm);
+        return "search";
+    }
 
 }
 
